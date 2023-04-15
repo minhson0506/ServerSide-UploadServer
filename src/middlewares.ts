fbb6f341a3c0c3ee7138c132a3ffaaea64d3075e
Son Dang <minhson0506@gmail.com>
2023-04-15 10:10:35 +0300

    init upload server

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -42,19 +42,20 @@ const authenticate = async (
 ) => {
   console.log('authenticate');
   try {
-    const response = await fetch(`${process.env.AUTH_URL}/api/v1/token`, {
-      headers: {
-        Authorization: req.headers.authorization || '',
-      },
-    });
-    if (!response.ok) {
-      throw new CustomError('Authentication failed', 401);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      next(new CustomError('Authentication failed', 401));
+      return;
     }
-    const message = await response.json();
-    const user = jwt.verify(message.token, process.env.JWT_SECRET as string);
-    res.locals.user = message.user;
+    const token = authHeader.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string);
+    if (!decodedToken) {
+      next(new CustomError('Authentication failed', 401));
+      return;
+    }
+    next();
   } catch (error) {
-    throw new CustomError('Authentication failed', 401);
+    next(new CustomError('Authentication failed', 401));
   }
 };
 
